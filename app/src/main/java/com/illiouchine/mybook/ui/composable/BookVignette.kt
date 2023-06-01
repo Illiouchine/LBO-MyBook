20e56e124cc280bbc5f74ad5fd9cf0df4bd84f2b
William <guillermin.w@gmail.com>
2023-06-01 16:25:32 +0200

    Add expandable feature on book vignette

--- app/src/main/java/com/illiouchine/mybook/ui/composable/BookVignette.kt
+++ app/src/main/java/com/illiouchine/mybook/ui/composable/BookVignette.kt
@@ -1,5 +1,7 @@
 package com.illiouchine.mybook.ui.composable
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.TweenSpec
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -7,15 +9,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -26,57 +27,98 @@ import com.illiouchine.mybook.feature.datagateway.entities.BookWithLikedEntity
 @Composable
 fun BookVignette(
     book: BookWithLikedEntity,
-    onBookClicked: (book:BookWithLikedEntity) -> Unit = {},
+    onBookClicked: (book: BookWithLikedEntity) -> Unit = {},
     onLikeClicked: (book: BookWithLikedEntity) -> Unit = {},
 ) {
-    Row {
-        Card(
-            modifier = Modifier.clickable {
+
+    var expandedState by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier
+            .clickable {
                 onBookClicked(book)
             }
-        ) {
-            Box{
-                Column {
-                    book.imageUrl?.let {
-                        val painter = rememberAsyncImagePainter(
-                            model = ImageRequest.Builder(LocalContext.current)
-                                .data(it.replace("http", "https")) // Todo Find a better placement to do the https thing
-                                .size(coil.size.Size.ORIGINAL) // Set the target size to load the image at.
-                                .build()
-                        )
-                        Image(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(200.dp)
-                                .background(Color.Yellow),
-                            painter = painter,
-                            contentDescription = null,
-                            contentScale = ContentScale.FillWidth
+            .animateContentSize(
+                animationSpec = TweenSpec(300)
+            )
+    ) {
+        Box {
+            Column {
+                book.imageUrl?.let {
+                    val painter = rememberAsyncImagePainter(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(
+                                it.replace(
+                                    "http",
+                                    "https"
+                                )
+                            ) // Todo Find a better placement to do the https thing
+                            .size(coil.size.Size.ORIGINAL) // Set the target size to load the image at.
+                            .build()
+                    )
+                    Image(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .background(Color.Yellow),
+                        painter = painter,
+                        contentDescription = null,
+                        contentScale = ContentScale.FillWidth
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = book.title,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    book.author?.let {
+                        Text(
+                            text = it,
+                            style = MaterialTheme.typography.titleMedium
                         )
                     }
-                    Column(
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .fillMaxWidth()
-                    ){
-                        Text(text = book.title)
-                        book.author?.let{
-                            Text(text = it)
+                    book.description?.let {
+                        if (expandedState) {
+                            Text(
+                                text = it,
+                            )
+                        } else {
+                            Text(
+                                text = it,
+                                maxLines = 2,
+                                overflow = TextOverflow.Ellipsis
+                            )
                         }
-                        book.description?.let{
-                            Text(text = it)
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        TextButton(
+                            modifier = Modifier.padding(4.dp),
+                            onClick = { expandedState = !expandedState },
+                        ) {
+                            if (expandedState) {
+                                Text(text = "see less")
+                            } else {
+                                Text(text = "see more")
+                            }
                         }
                     }
                 }
-
-                LikeIcon(
-                    modifier = Modifier.align(Alignment.TopEnd),
-                    liked = book.liked,
-                    onLikeClicked = {
-                        onLikeClicked(book)
-                    }
-                )
             }
+
+            LikeIcon(
+                modifier = Modifier.align(Alignment.TopEnd),
+                liked = book.liked,
+                onLikeClicked = {
+                    onLikeClicked(book)
+                }
+            )
         }
     }
 }
@@ -86,9 +128,9 @@ fun BookVignette(
 fun LikeIcon(
     modifier: Modifier = Modifier,
     liked: Boolean,
-    onLikeClicked : () -> Unit = {}
+    onLikeClicked: () -> Unit = {}
 ) {
-    if ( liked ){
+    if (liked) {
         Icon(
             modifier = modifier
                 .padding(8.dp)
@@ -98,7 +140,7 @@ fun LikeIcon(
             contentDescription = "Remove to Favorite",
             tint = Color.Red
         )
-    }else {
+    } else {
         Icon(
             modifier = modifier
                 .padding(8.dp)
@@ -113,26 +155,42 @@ fun LikeIcon(
 @Preview
 @Composable
 fun PrevBookVignette() {
-    BookVignette(book = BookWithLikedEntity(
-        id = "id",
-        title = "Title",
-        author = "Author",
-        description = "Une super longue description",
-        imageUrl = "ImageUrl",
-        liked = false
-    ))
+    BookVignette(
+        book = BookWithLikedEntity(
+            id = "id",
+            title = "Title",
+            author = "Author",
+            description = "Oscillation within a space time matrix, with respect to hyperbolic sub-resonant variations, combined with total holistic resonant emanations calling forth spiritual entrainment between sentient beings.\n" +
+                    "\n" +
+                    "This technology combines quantum dialectic projections, bonded on a quantum level with a higher order harmonic series calling forth a higher order contextual latency.\n" +
+                    "\n" +
+                    "Focusing on a single point in space-time can lead to an understanding of stabilized vibrational resonance, bonded on a quantum level with geodesic lattice structures in order to generate a psychic connection to the reality plane.\n" +
+                    "\n" +
+                    "The ability to fold space within the reality envelope facilitates the ability to make use of holographic temporal resonance, interwoven on a molecular level with isotropic transfer functions manifesting clear connection to the Akashic Records.",
+            imageUrl = "ImageUrl",
+            liked = false
+        )
+    )
 }
 
 
 @Preview
 @Composable
 fun PrevBookVignette2() {
-    BookVignette(book = BookWithLikedEntity(
-        id = "id",
-        title = "Title",
-        author = "Author",
-        description = "Une super longue description",
-        imageUrl = "ImageUrl",
-        liked = true
-    ))
+    BookVignette(
+        book = BookWithLikedEntity(
+            id = "id",
+            title = "Title",
+            author = "Author",
+            description = "Oscillation within a space time matrix, with respect to hyperbolic sub-resonant variations, combined with total holistic resonant emanations calling forth spiritual entrainment between sentient beings.\n" +
+                    "\n" +
+                    "This technology combines quantum dialectic projections, bonded on a quantum level with a higher order harmonic series calling forth a higher order contextual latency.\n" +
+                    "\n" +
+                    "Focusing on a single point in space-time can lead to an understanding of stabilized vibrational resonance, bonded on a quantum level with geodesic lattice structures in order to generate a psychic connection to the reality plane.\n" +
+                    "\n" +
+                    "The ability to fold space within the reality envelope facilitates the ability to make use of holographic temporal resonance, interwoven on a molecular level with isotropic transfer functions manifesting clear connection to the Akashic Records.",
+            imageUrl = "ImageUrl",
+            liked = true
+        )
+    )
 }
